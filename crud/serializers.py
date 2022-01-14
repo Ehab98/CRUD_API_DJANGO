79b181efd7baaa832b2f16c1e07e25dd06d9e3b5
Ehab98 <hoba.reda98@gmail.com>
2022-01-14 22:58:17 +0200

    serializers

---
+++ crud/serializers.py
@@ -0,0 +1,27 @@
+from dataclasses import fields
+from pyexpat import model
+from rest_framework import serializers
+
+from .models import student , course , Teacher , place
+
+class StudentSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = student
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = course
+        fields ='__all__'
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Teacher
+        fields='__all__'
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=place
+        fields='__all__'
+        
